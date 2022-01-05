dd5348c31fa3462100c635b8db14af5f2333940c
gluay88 <p.wantagaew@gmail.com>
2022-01-05 10:11:47 -0800

    Here we go

--- src/App.js
+++ src/App.js
@@ -6,15 +6,59 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      playerTurn: null,
     }
   }
 
+  handleGamePlay = (index) => {
+    const { squares, playerTurn } = this.state
+    if(squares[index] === 0){
+      squares[index] = playerTurn
+    } 
+
+    playerTurn === "❌" ? this.oFirst() : this.xFirst()
+
+    this.setState({squares: squares})
+  }
+
+
+
+  xFirst = () => {
+    this.setState({
+      playerTurn: "❌"
+    })
+  }
+
+  oFirst = () => {
+    this.setState({
+      playerTurn: "⭕️"
+    })
+  }
+
+  
   render(){
     return(
       <>
-        <h1>Tic Tac Toe</h1>
-        <Square />
+        <h1>Noughts-and-Crosses</h1>
+        <div className="gameboard">
+          {this.state.squares.map((value, index) => {
+            return (
+              value=<Square 
+              value={value}
+              index={index}
+              key={index}
+              handleGamePlay={this.handleGamePlay}
+              />
+            )
+          })}
+
+        </div>
+
+        <h1>Who's going first?</h1>
+        <button onClick={this.xFirst}>X</button>
+        <button onClick={this.oFirst}>0</button>
+        
       </>
     )
   }
